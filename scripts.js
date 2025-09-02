df6d210dd0f192d02f6aeb2a6c48429b4f51e15d
SirAlexanator <aredinger90@gmail.com>
2025-09-01 23:35:11 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
 
+  const meleeAttack1Input = document.getElementById('MA-1');
+  const priceOutput1 = document.getElementById('P-1');
+
+  meleeAttack1Input.addEventListener('input', () => {
+    calcPrice(2);
+});
+});
+function calcPrice(rowNumber){
+const price = document.getElementById(`P-${rowNumber}`);
+price.textContent = '1';
+}
+
+function updatePrice(rowNumber, change){
+const price = document.getElementById(`P-${rowNumber}`);
+let newPrice = price;
+newPrice = newPrice+change;
+price.textContent = `${newPrice}`;
+}
+
+function calcCategory(rowNumber){
+    if()
+}
